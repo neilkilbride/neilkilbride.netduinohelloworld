c9a980f416c8037a6979260ca5e5b335ab8cd1b6
Neil Kilbride <neil@neilkilbride.com>
2011-12-04 18:21:01 +0000

    First commit :)

---
+++ neilkilbride.netduinohelloworld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace neilkilbride.netduinohelloworld
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var led = new OutputPort(Pins.ONBOARD_LED, false);
+
+            while (true)
+            {
+                led.Write(!led.Read());
+
+                Thread.Sleep(1000);
+            }
+        }
+
+    }
+}
